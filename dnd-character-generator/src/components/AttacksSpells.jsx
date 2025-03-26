93b49b045db8cfb725aabc05c7a5142ecb017816
MarshaZeldenrust <marshazeldenrust@gmail.com>
2025-03-26 21:01:41 +0100

    lay-out opgezet en character info en ability score generatie werkend gemaakt

---
+++ dnd-character-generator/src/components/AttacksSpells.jsx
@@ -0,0 +1,7 @@
+export function AttacksSpells() {
+    return (
+      <div className="p-2 border border-black bg-[#e1d3b8] text-center font-bold">
+        AttacksSpells
+      </div>
+    );
+  }
---
+++ dnd-character-generator/src/utils/GenerateCharacterInfo.js
@@ -0,0 +1,52 @@
+//Genereert de Character Info
+export async function generateCharacterData() {
+    const [raceRes, classRes, backgroundsRes] = await Promise.all([
+      fetch('https://www.dnd5eapi.co/api/races'),
+      fetch('https://www.dnd5eapi.co/api/classes'),
+      fetch('https://www.dnd5eapi.co/api/backgrounds')
+    ]);
+  
+    const [races, classes, backgrounds] = await Promise.all([
+      raceRes.json(),
+      classRes.json(),
+      backgroundsRes.json()
+    ]);
+  
+    // De API heeft voor backgrounds alleen Acolyte staan, dus die vul ik aan
+
+    const apiBackgrounds = backgrounds.results.map(b => b.name);
+
+    // new Set zorgt ervoor dat er geen duplicates voorkomen voor het geval de API ooit aangevuld wordt
+    const combinedBackgrounds = [...new Set([
+        ...apiBackgrounds,
+        "Folk Hero",
+        "Sage",
+        "Soldier",
+        "Criminal",
+        "Entertainer",
+        "Noble",
+        "Outlander",
+        "Hermit",
+        "Guild Artisan"
+      ])];
+      
+    const randomRace = races.results[Math.floor(Math.random() * races.results.length)].index;
+    const randomClass = classes.results[Math.floor(Math.random() * classes.results.length)].index;
+    const randomBackground = combinedBackgrounds[Math.floor(Math.random() * combinedBackgrounds.length)];
+    const randomAlignment = [
+      "Lawful Good", "Neutral Good", "Chaotic Good",
+      "Lawful Neutral", "True Neutral", "Chaotic Neutral",
+      "Lawful Evil", "Neutral Evil", "Chaotic Evil"
+    ][Math.floor(Math.random() * 9)];
+  
+    const raceData = await fetch(`https://www.dnd5eapi.co/api/races/${randomRace}`).then(res => res.json());
+    const classData = await fetch(`https://www.dnd5eapi.co/api/classes/${randomClass}`).then(res => res.json());
+  
+    return {
+      race: raceData.name,
+      charClass: classData.name,
+      background: randomBackground,
+      alignment: randomAlignment
+    };
+  }
+  
